2b9e594af9afdbaf73e80d6cea171409e75c785a
taehyun4882 <m63904882@gmail.com>
2025-07-21 15:19:34 +0900

    Create loop02.c

---
+++ C/Basic/loop02.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+//while 문을 이용해서 100부터 0까지 입력
+	//int i = 100;
+	//while (i>=0) {
+	//	printf("%d\n", i);
+	//	i--;
+	//	// i-=1;
+	//	// i=i-1;
+
+	//}
+	// for문을 사용해서 100부터 0까지 출력하세요.
+	for (int i = 100; i >= 0; i--) {
+		printf("%d ", i);
+			}
+	printf("\n ");
+	return 0;
+}
